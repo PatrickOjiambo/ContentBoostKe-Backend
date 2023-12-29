c714077c97d2555a83a48912f114373933f80d28
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-12-29 22:39:46 +0300

    feat: init frontend

--- backend/models/createdb.js
+++ backend/models/createdb.js
@@ -1,11 +1,3 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
-dotenv.config();
-export const pool = mysql.createPool({
-  host: process.env.DATABASEHOST,
-  user: process.env.DATABASEUSER, 
-  password: process.env.DATABASEPASSWORD,
-  database: process.env.DATABASENAME
-});
-
-const promiseConnection = pool.promise();
+const saveDonor = ()=>{
+  
+}
